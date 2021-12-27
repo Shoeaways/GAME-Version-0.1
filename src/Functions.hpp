ec876efaddace3d1ddd021c8bbe5814a21d9f322
Eric Shu <eshu2146@gmail.com>
2021-12-26 17:06:15 -0800

    Test

--- src/Functions.hpp
+++
@@ -1,9 +0,0 @@
-//Using this file to Include all necessary .hpp files to shrink size when including in Main
-//TODO -> Add all #includes
-
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
-
-#include "GameState/GameState.hpp"
-
-#endif
